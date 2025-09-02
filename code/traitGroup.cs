592b8f9789dae2d7056d94f708ba7be5a402b23d
兔子 <413990019@qq.com>
2025-09-02 23:22:10 +0800

    fix rarity enum names for early achievements

--- code/traitGroup.cs
+++ code/traitGroup.cs
@@ -24,6 +24,31 @@ namespace DemonGameRules.code
             demonGroup.name = "ASCENDED_DEMON_GROUP";  // 本地化键
             demonGroup.color = "#8B0000";              // 暗红色
             AssetManager.trait_groups.add(demonGroup);
+
+            // ====== 成就篇章特质组 ======
+            ActorTraitGroupAsset mortalCoilGroup = new ActorTraitGroupAsset();
+            mortalCoilGroup.id = "mortal_coil_group";
+            mortalCoilGroup.name = "MORTAL_COIL_GROUP";
+            mortalCoilGroup.color = "#CD7F32"; // bronze
+            AssetManager.trait_groups.add(mortalCoilGroup);
+
+            ActorTraitGroupAsset destinyGroup = new ActorTraitGroupAsset();
+            destinyGroup.id = "destiny_group";
+            destinyGroup.name = "DESTINY_GROUP";
+            destinyGroup.color = "#4169E1"; // royal blue
+            AssetManager.trait_groups.add(destinyGroup);
+
+            ActorTraitGroupAsset beyondMortalityGroup = new ActorTraitGroupAsset();
+            beyondMortalityGroup.id = "beyond_mortality_group";
+            beyondMortalityGroup.name = "BEYOND_MORTALITY_GROUP";
+            beyondMortalityGroup.color = "#4B0082"; // indigo
+            AssetManager.trait_groups.add(beyondMortalityGroup);
+
+            ActorTraitGroupAsset endPathGroup = new ActorTraitGroupAsset();
+            endPathGroup.id = "end_path_group";
+            endPathGroup.name = "END_PATH_GROUP";
+            endPathGroup.color = "#FF4500"; // orange red
+            AssetManager.trait_groups.add(endPathGroup);
         }
     }
 }
